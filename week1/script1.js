16a62525587693c3b5c4e6759336dad323fd6b68
mjsakasaka <161197680+mjsakasaka@users.noreply.github.com>
2024-04-03 14:29:26 +0800

    Add files via upload

---
+++ week1/script1.js
@@ -0,0 +1,7 @@
+document.getElementById("burger").addEventListener("click", function() {
+    document.getElementById("popup-menu").style.display = "block";
+});
+
+document.getElementById("close-btn").addEventListener("click", function() {
+    document.getElementById("popup-menu").style.display = "none";
+});
